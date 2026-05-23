6bbf02348151c49f0af1239f171fd7dfc88fad98
Adrien Poupard <adrien@komune.io>
2026-05-23 21:19:55 +0200

    chore(sdk): delete commented-out ssm-sdk-dsl InvokeReturn stub
    The file held only a commented-out data class declaration (the real one
    lived in chaincode-dsl and was deleted in the V2 cleanup). Removing the
    file too.

--- c2-ssm/ssm-sdk/ssm-sdk-dsl/src/commonMain/kotlin/ssm/sdk/dsl/InvokeReturn.kt
+++
@@ -1,9 +0,0 @@
-package ssm.sdk.dsl
-
-import io.komune.c2.chaincode.dsl.TransactionId
-
-//data class InvokeReturn(
-//    val status: String,
-//    val info: String,
-//    val transactionId: TransactionId,
-//)
